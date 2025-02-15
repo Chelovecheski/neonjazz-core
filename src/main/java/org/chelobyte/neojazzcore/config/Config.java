53fce127fa32534ae72aaece98b87de7b0413ca2
Chelovecheski <yaroslav05040713@gmail.com>
2025-02-15 13:18:05 +0100

    Initial commit

---
+++ src/main/java/org/chelobyte/neojazzcore/config/Config.java
@@ -0,0 +1,9 @@
+package org.chelobyte.neojazzcore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class Config {
+}
